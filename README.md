0c04a2d7afd3c115cf764ad3bd447f787d085d64
OssFate <mantis1512@gmail.com>
2019-06-18 17:43:11 -0500

    Better understanding of Vue Gotta start working on making this functional, it gotta work as a cms

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # OssBlog
+
 Blog for firebase, with NodeJS and VueJS
+
+## TODO
+
+- [ ] Admin View
+  - [ ] Admin URL
+  - [ ] Add new posts
+  - [ ] Preview posts
+- [ ] Better view
+- [ ] Favicon
--- oss_blog_vue/src/components/OssBlog.vue
+++ oss_blog_vue/src/components/OssBlog.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
+  <div class="ossblog">
+    <h1>{{ titleMsg }}</h1>
+    <h3>{{ message }}</h3>
+    <h3>{{ snapVal }}</h3>
+    <button @click="getData()">Get Data</button>
+    <!-- <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -26,16 +29,54 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul>-->
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from "vue-property-decorator";
+import * as firebase from "firebase/app";
+
+import "firebase/database";
+
+const config = {
+  apiKey: "AIzaSyD54K7VBHkU1tSAAymI1JPmnS7lvmmukEM",
+  authDomain: "ossbio-d6647.firebaseapp.com",
+  databaseURL: "https://ossbio-d6647.firebaseio.com"
+};
+firebase.initializeApp(config);
 
 @Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+export default class OssBlog extends Vue {
+  public message: string = "Hello World!";
+
+  @Prop() private titleMsg!: string;
+
+  private db: firebase.database.Database = firebase.database();
+  private testRef: firebase.database.Reference = this.db.ref("posts");
+
+  private snapVal = [];
+
+  public async setData() {
+    await this.testRef.set({
+      name: "Oswald Mantilla",
+      testing: "RealNames",
+      array: ["one", "two", "three"]
+    });
+  }
+
+  public async getData() {
+    this.testRef.on("value", (a, b) => {
+      //   console.log(a.val());
+      this.snapVal = a.val();
+      //   console.log(b);
+    });
+    // this.snapVal = snap.val();
+  }
+
+  private mounted() {
+    this.getData();
+  }
 }
 </script>
 
